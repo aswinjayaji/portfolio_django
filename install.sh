4ae5dc295d5703f629c84d3ad15e01d8c5a86ac9
Aswin Jayaji <56126732+aswinjayaji@users.noreply.github.com>
2021-05-04 20:02:20 +0530

    commit

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Let's call this script venv.sh
-source "/mnt/d/websites/bin/activate"
--- portfolio_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import works
-from .models import price 
-# Register your models here.
-
-
-admin.site.register(works)
-admin.site.register(price)
